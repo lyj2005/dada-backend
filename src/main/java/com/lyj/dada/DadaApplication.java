644992a84ef96e1cd46116fdc62fa893d61959a2
lyj <820140968@qq.com>
2026-02-23 08:51:13 +0800

    2. 初始化项目

--- src/main/java/com/lyj/dada/DadaApplication.java
+++ src/main/java/com/lyj/dada/DadaApplication.java
@@ -1,9 +1,11 @@
 package com.lyj.dada;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lyj.dada.mapper")
 public class DadaApplication {
 
     public static void main(String[] args) {
